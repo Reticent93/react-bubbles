1eb5512655dc4019da0eafe0a50be6258e9b660e
Greg <remph819@gmail.com>
2020-01-24 17:46:11 -0700

    made api call in bubblepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,26 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Login from "./components/Login";
-import "./styles.scss";
+import Login from './components/Login';
+import ProtectedRoute from './components/BubblePage';
+import './styles.scss';
+import BubblePage from './components/BubblePage';
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
+	return (
+		<Router>
+			<div className="App">
+				<Route exact path="/" component={Login} />
+				{
+					/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
-        */}
-      </div>
-    </Router>
-  );
+        */
+					<ProtectedRoute exact path="/bubblePage" component={BubblePage} />
+				}
+			</div>
+		</Router>
+	);
 }
 
 export default App;
--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,20 +1,30 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import api from '../utils/api';
 
-import Bubbles from "./Bubbles";
-import ColorList from "./ColorList";
+import Bubbles from './Bubbles';
+import ColorList from './ColorList';
 
-const BubblePage = () => {
-  const [colorList, setColorList] = useState([]);
-  // fetch your colors data from the server when the component mounts
-  // set that data to the colorList state property
+const BubblePage = (props) => {
+	const [ colorList, setColorList ] = useState([]);
+	// fetch your colors data from the server when the component mounts
+	// set that data to the colorList state property
+	useEffect(() => {
+		api()
+			.get('/api/colors')
+			.then((res) => {
+				setColorList(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
 
-  return (
-    <>
-      <ColorList colors={colorList} updateColors={setColorList} />
-      <Bubbles colors={colorList} />
-    </>
-  );
+	return (
+		<div>
+			<ColorList colors={colorList} updateColors={setColorList} />
+			<Bubbles colors={colorList} />
+		</div>
+	);
 };
 
 export default BubblePage;
